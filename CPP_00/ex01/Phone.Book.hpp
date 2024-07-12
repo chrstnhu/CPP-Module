7462a6f39d182db0bc29be122df71636977333c6
Christine Hu <chrhu@paul-f4Ar3s2.clusters.42paris.fr>
2024-07-12 19:08:23 +0200

    add .h file

---
+++ CPP_00/ex01/Phone.Book.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phone.Book.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 15:28:02 by chrhu             #+#    #+#             */
+/*   Updated: 2024/07/12 19:01:02 by chrhu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONE_BOOK_H
+# define PHONE_BOOK_H
+
+#include <iostream>
+#include <string>
+
+# define DEFAULT "\033[0;39m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m"
+
+class PhoneBook 
+{
+	public :
+
+};
+
+class Contact
+{
+	private :
+		std::string firstName;
+		std::string lastName;
+		std::string phoneNumber;
+	public :
+
+};
+
+#endif
