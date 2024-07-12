cc507199ad6ec607e706d3c51ed8affd8a743bd7
Christine Hu <chrhu@paul-f4Ar3s2.clusters.42paris.fr>
2024-07-12 19:07:52 +0200

    add main

---
+++ CPP_00/ex01/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 15:27:53 by chrhu             #+#    #+#             */
+/*   Updated: 2024/07/12 18:17:27 by chrhu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Phone.Book.hpp"
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	std::string str;
+
+	if (argc == 1)
+	{
+		std::cout << "> ";
+		while (std::getline(std::cin, str))
+		{
+			if (str == "EXIT")
+				break ;
+			else if (str == "ADD")
+				std::cout << "enter ADD" << std::endl;
+			else if (str == "SEARCH")
+				std::cout << "enter SEARCH" << std::endl;
+			else
+				std::cout << RED << "Try again with : ADD/SEARCH/EXIT " << DEFAULT << std::endl;
+			std::cout << "> ";
+		}
+	}
+	return (0);
+}
