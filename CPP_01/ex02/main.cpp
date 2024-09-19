aed33710b1d1bc9f1f129ed48726c01041cb3847
Christine Hu <chrhu@paul-f5Br3s6.clusters.42paris.fr>
2024-09-19 16:44:21 +0200

    add cpp01 ex02

---
+++ CPP_01/ex02/main.cpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 10:16:48 by chrhu             #+#    #+#             */
+/*   Updated: 2024/09/19 16:43:48 by chrhu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+# define DEF "\033[0;39m"
+# define YELLOW "\033[0;33m" 
+
+int main () {
+	std::string str("HI THIS IS BRAIN");
+	std::string str2("Hello");
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << YELLOW << "--- Print str ---" << DEF << std::endl;
+	std::cout << str << std::endl;
+	
+	std::cout << YELLOW << "--- Print memory adress ---" << DEF << std::endl;
+	std::cout << "   string: " << &str << std::endl;
+	std::cout << "stringPTR: " << stringPTR << std::endl;
+	std::cout << "stringREF: " << &stringREF << std::endl;
+
+	std::cout << YELLOW << "--- Print value ---" << DEF << std::endl;
+	std::cout << "   string: " << str << std::endl;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	std::cout << "stringREF: " << stringREF << std::endl;
+	
+	std::cout << YELLOW << "--- Change ---" << DEF << std::endl;
+	std::cout << "string: " << str << std::endl;
+	*stringPTR = "Can I change the value with stringPTR ?";
+	std::cout << "string: " << str << std::endl;
+	stringREF = "Can I also change stringREF ?";
+	std::cout << "string: " << str << std::endl;
+	str = "HI THIS IS BRAIN ?";
+	std::cout << "string: " << str << std::endl;
+
+	std::cout << YELLOW << "--- Reaffecter ---" << DEF << std::endl;
+	stringPTR = &str2;
+	std::cout << "stringPTR: " << *stringPTR << std::endl;
+	// stringREF = &str2;
+	std::cout << "stringREF: " << stringREF << std::endl;
+}
