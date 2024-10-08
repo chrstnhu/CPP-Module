0c9e6f4d826dc4a6e7e059aae63234bf1f670e7a
chrhu <chrhu@student.42.fr>
2024-10-08 17:46:17 +0200

    modif delete

--- CPP_01/ex01/main.cpp
+++ CPP_01/ex01/main.cpp
@@ -6,34 +6,35 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 18:56:11 by chrhu             #+#    #+#             */
-/*   Updated: 2024/10/05 14:31:25 by chrhu            ###   ########.fr       */
+/*   Updated: 2024/10/08 16:02:16 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 int main(int ac, char **av) {
-	(void)ac;
-	if (av[1] != NULL)
-	{
-		std::cout << YELLOW << " Usage : ./zombieHorde " << DEF << std::endl;
-		return (1);
-	}
+    (void)ac;
+    if (av[1] != NULL)
+    {
+        std::cout << YELLOW << " Usage : ./zombieHorde " << DEF << std::endl;
+        return (1);
+    }
 
-	int i = 0;
-	const int nb = 5;
-	std::string names[nb] = {"Foo", "New", "John"};
-	
-	Zombie *zombie;
+    const int nb = 5; 
+    std::string names[nb] = {"Foo", "New", "John"};
+    Zombie** zombieHordes = new Zombie*[nb];
 
-	std::cout << YELLOW << " ---Zombie Horde--- " << DEF << std::endl;	
-	while (i < nb)
-	{
-		zombie = zombieHorde(1, names[i]);
-		zombie[0].announce();
-		delete [](zombie);
-		i++;
-	}
+	std::cout << YELLOW << " ---Zombie Horde--- " << DEF << std::endl;    
+	for (int i = 0 ; i < nb ; i++)
+    {
+        zombieHordes[i] = zombieHorde(1, names[i]);
+        zombieHordes[i][0].announce();
+    }
+
+    for (int j = 0; j < nb; j++) {
+        delete[] zombieHordes[j];
+    }
+    delete[] zombieHordes;
+
     return (0);
 }
-
