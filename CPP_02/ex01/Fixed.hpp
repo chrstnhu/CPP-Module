62240bdb4853a5c29644f684d105b19fb1355e15
chrhu <chrhu@student.42.fr>
2024-10-11 17:01:39 +0200

    add cpp02 ex01

---
+++ CPP_02/ex01/Fixed.hpp
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/10 19:23:54 by chrhu             #+#    #+#             */
+/*   Updated: 2024/10/11 17:01:02 by chrhu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+# define BLUE "\033[0;313m"
+# define DEF "\033[0;39m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m" 
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+	private :
+		int					_raw;
+		static int const 	_rawBit = 8;
+		static float const	_rawFloatBit;
+	
+	public :
+		Fixed();
+		Fixed( int number );
+		Fixed( float number );
+		Fixed( const Fixed &other );
+		~Fixed();
+		
+		Fixed	&operator=( const Fixed &other );
+
+		
+		int		getRawBits( void ) const;
+		void	setRawBits( int const raw );
+
+		float	toFloat( void ) const;
+		int		toInt( void ) const;
+
+		friend std::ostream &operator << (std::ostream& os, const Fixed &obj);
+};
+
+#endif
