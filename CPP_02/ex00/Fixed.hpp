f06f21f79f14c072e2a1ca3120d1740a80f0b2ba
chrhu <chrhu@student.42.fr>
2024-10-11 17:02:10 +0200

    change exec name

--- CPP_02/ex00/Fixed.hpp
+++ CPP_02/ex00/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/10 19:23:54 by chrhu             #+#    #+#             */
-/*   Updated: 2024/10/11 14:30:37 by chrhu            ###   ########.fr       */
+/*   Updated: 2024/10/11 16:00:55 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 # define YELLOW "\033[0;33m" 
 
 #include <iostream>
-#include <cmath>
 
 class Fixed {
 	private :
