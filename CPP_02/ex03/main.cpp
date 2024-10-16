625da2d510bb02b9b7f1ab2be6350e8dbda4fa06
chrhu <chrhu@student.42.fr>
2024-10-16 11:08:59 +0200

    change main test

--- CPP_02/ex03/main.cpp
+++ CPP_02/ex03/main.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/10 19:23:01 by chrhu             #+#    #+#             */
-/*   Updated: 2024/10/15 17:21:14 by chrhu            ###   ########.fr       */
+/*   Created: 2024/10/16 11:05:38 by chrhu             #+#    #+#             */
+/*   Updated: 2024/10/16 11:06:44 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,35 +17,34 @@ static void print_bsp(Point &a, Point &b, Point &c, Point point);
 
 int main(void) {
 	// Triangle coordinates
-    Point a(0, 0);
-    Point b(5, 0);
-    Point c(0, 5);
+	Point a(0, 0);
+	Point b(5, 0);
+	Point c(0, 5);
 
+	std::cout << "Triangle coordinates = a(0, 0); b(5, 0); c(0, 5)" << std::endl << std::endl;
+	
 	//Points
-    Point p1(1, 1);   // Inside
-    Point p2(6.6, 1);   // Outside
-    Point p3(0, 0);   // On the edge
-    Point p4(2, 0);   // On the edge
-	Point p5(1.658, 2.5);   // Inside;
+	Point p1(1, 1);
+	Point p2(1.658, 2.5);
+	Point p3(0, 0);
+	Point p4(2, 0);
+	Point p5(6.6, 1);
 
-	std::cout << "Test pour point (1, 1): ";
-    print_bsp(a, b, c, p1);
-	std::cout << "Test pour point (6.6, 1): ";
+	print_bsp(a, b, c, p1);
 	print_bsp(a, b, c, p2);
-	std::cout << "Test pour point (0, 0): ";
 	print_bsp(a, b, c, p3);
-	std::cout << "Test pour point (2, 0): ";
 	print_bsp(a, b, c, p4);
-	std::cout << "Test pour point (1.658, 2.5): ";
 	print_bsp(a, b, c, p5);
-    return 0;
+	return 0;
 }
 
 static void print_bsp(Point &a, Point &b, Point &c, Point point)
 {
 	if (bsp(a, b, c, point)) {
-        std::cout << "À l'intérieur" << std::endl;
-    } else {
-        std::cout << "À l'extérieur" << std::endl;
-    }
+		std::cout << "Test for point (" << point.getX() << ", " << point.getY() << "): ";
+		std::cout << "Inside the triangle" << std::endl;
+	} else {
+		std::cout << "Test for point (" << point.getX() << ", " << point.getY() << "): ";
+		std::cout << "Outside the triangle" << std::endl;
+	}
 }
