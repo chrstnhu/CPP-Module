61770642cfb3f49e2edf576784f6eb1616b5fb54
chrhu <chrhu@student.42.fr>
2024-10-17 18:34:59 +0200

    change type

--- CPP_01/ex03/Weapon.cpp
+++ CPP_01/ex03/Weapon.cpp
@@ -6,7 +6,7 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 14:29:37 by chrhu             #+#    #+#             */
-/*   Updated: 2024/10/09 16:51:06 by chrhu            ###   ########.fr       */
+/*   Updated: 2024/10/17 16:57:42 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ Weapon::~Weapon() {
 };
 
 // Set type
-void Weapon::setType(std::string newType) {
+void Weapon::setType(std::string type) {
 	_type = type;
 }
 
