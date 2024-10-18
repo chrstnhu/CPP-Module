feb91f06cb4a40b56655423297c6b42287a62178
chrhu <chrhu@student.42.fr>
2024-10-18 12:53:11 +0200

    add color

--- CPP_03/ex00/ClapTrap.hpp
+++ CPP_03/ex00/ClapTrap.hpp
@@ -6,12 +6,18 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 18:20:58 by chrhu             #+#    #+#             */
-/*   Updated: 2024/10/17 18:30:16 by chrhu            ###   ########.fr       */
+/*   Updated: 2024/10/18 12:53:03 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
 # define CLAPTRAP_HPP
+
+# define BLUE "\033[0;313m"
+# define DEF "\033[0;39m"
+# define RED "\033[0;31m"
+# define GREEN "\033[0;32m"
+# define YELLOW "\033[0;33m" 
 
 #include <iostream>
 #include <cmath>
