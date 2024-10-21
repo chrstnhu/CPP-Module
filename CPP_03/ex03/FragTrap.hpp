fd6d744dc41027593f7b8359d5fe0da6056235eb
chrhu <chrhu@student.42.fr>
2024-10-21 16:33:11 +0200

    add virtual

--- CPP_03/ex03/FragTrap.hpp
+++ CPP_03/ex03/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/18 18:15:42 by chrhu             #+#    #+#             */
-/*   Updated: 2024/10/21 11:36:39 by chrhu            ###   ########.fr       */
+/*   Updated: 2024/10/21 13:14:59 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 #include "ClapTrap.hpp"
 
-class FragTrap : public ClapTrap {
+class FragTrap : virtual public ClapTrap {
 	public :
 		FragTrap();
 		FragTrap( std::string name );
