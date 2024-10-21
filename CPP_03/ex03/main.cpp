19a9d859ce90c277a4fff17dd7bda3b12d6307a1
chrhu <chrhu@student.42.fr>
2024-10-21 16:32:21 +0200

    add test

--- CPP_03/ex03/main.cpp
+++ CPP_03/ex03/main.cpp
@@ -6,26 +6,28 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 18:27:07 by chrhu             #+#    #+#             */
-/*   Updated: 2024/10/18 18:41:20 by chrhu            ###   ########.fr       */
+/*   Updated: 2024/10/21 13:23:58 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
+#include "DiamondTrap.hpp"
 
 static void printIdentity(ClapTrap &Clap);
-static void ftAttack(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap);
-static void ftTakeDamage(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap);
-static void ftRepair(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap);
+static void ftAttack(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap, DiamondTrap &DiamondTrap);
+static void ftTakeDamage(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap, DiamondTrap &DiamondTrap);
+static void ftRepair(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap, DiamondTrap &DiamondTrap);
 
 int main () {
 	ClapTrap ClapTrap("Bob");
 	ScavTrap ScavTrap;
 	FragTrap FragTrap("Knight");
+	DiamondTrap DiamondTrap("Diamond");
 
-	ftAttack(ClapTrap, ScavTrap, FragTrap);
-	ftTakeDamage(ClapTrap, ScavTrap, FragTrap);
-	ftRepair(ClapTrap, ScavTrap, FragTrap);
+	ftAttack(ClapTrap, ScavTrap, FragTrap, DiamondTrap);
+	ftTakeDamage(ClapTrap, ScavTrap, FragTrap, DiamondTrap);
+	ftRepair(ClapTrap, ScavTrap, FragTrap, DiamondTrap);
 
 	// Gate keeper mode
 	std::cout << std::endl << "======================= Guard Gate =======================" << std::endl;
@@ -34,9 +36,14 @@ int main () {
 	// High Five
 	std::cout << std::endl << "======================= High Five =======================" << std::endl;
 	FragTrap.highFivesGuys();
+
+	// Diamond Trap
+	std::cout << std::endl << "======================= Diamond trap =======================" << std::endl;
+	printIdentity(DiamondTrap);
+	DiamondTrap.whoAmI();
 }
 
-static void ftAttack(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap) {
+static void ftAttack(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap, DiamondTrap &DiamondTrap) {
 	std::cout << std::endl << "======================= Attack =======================" << std::endl;
 	ClapTrap.attack("Someone");
 	ClapTrap.attack("Zombie");
@@ -49,9 +56,12 @@ static void ftAttack(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap)
 	for (int i = 0; i < 5; i++)
 		FragTrap.attack("robot");
 	printIdentity(FragTrap);
+
+	DiamondTrap.attack("rock");
+	printIdentity(DiamondTrap);
 }
 
-static void ftTakeDamage(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap) {
+static void ftTakeDamage(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap, DiamondTrap &DiamondTrap) {
 	std::cout << std::endl << "======================= Take Damage =======================" << std::endl;
 	ClapTrap.takeDamage(10);
 	printIdentity(ClapTrap);
@@ -65,9 +75,12 @@ static void ftTakeDamage(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragT
 
 	FragTrap.takeDamage(95);
 	printIdentity(FragTrap);
+
+	DiamondTrap.takeDamage(80);
+	printIdentity(DiamondTrap);
 }
 
-static void ftRepair(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap) {
+static void ftRepair(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap, DiamondTrap &DiamondTrap) {
 	std::cout << std::endl << "======================= Repair =======================" << std::endl;
 	ClapTrap.beRepaired(7);
 	printIdentity(ClapTrap);
@@ -79,6 +92,9 @@ static void ftRepair(ClapTrap &ClapTrap, ScavTrap &ScavTrap, FragTrap &FragTrap)
 
 	FragTrap.beRepaired(100);
 	printIdentity(FragTrap);
+
+	DiamondTrap.beRepaired(50);
+	printIdentity(DiamondTrap);
 }
 
 static void printIdentity(ClapTrap &Clap)
@@ -89,6 +105,8 @@ static void printIdentity(ClapTrap &Clap)
 			<< " - Hit point(s): " 
 			<< YELLOW << Clap.getHitPoint() << DEF
 			<< ", energy point: "
-			<< YELLOW << Clap.getEnergyPoint() << DEF << std::endl;
+			<< YELLOW << Clap.getEnergyPoint() << DEF
+			<< ", attack damage: "
+			<< YELLOW << Clap.getAttackDamage() << DEF << std::endl;
 	}
 }
