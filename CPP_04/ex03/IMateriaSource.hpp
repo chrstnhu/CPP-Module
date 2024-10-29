04e6b8bf901bd9bbed817fb128ef52969f033f4d
chrhu <chrhu@student.42.fr>
2024-10-29 16:37:02 +0100

    add interface hpp

--- CPP_04/ex03/IMateriaSource.hpp
+++ CPP_04/ex03/IMateriaSource.hpp
@@ -1,27 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*   IMateriaSource.hpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/29 12:26:11 by chrhu             #+#    #+#             */
-/*   Updated: 2024/10/29 16:23:05 by chrhu            ###   ########.fr       */
+/*   Created: 2024/10/29 13:11:29 by chrhu             #+#    #+#             */
+/*   Updated: 2024/10/29 16:32:19 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ICHARACTER_HPP
-# define ICHARACTER_HPP
+#ifndef IMATERIASOURCE_HPP
+# define IMATERIASOURCE_HPP
 
 #include "AMateria.hpp"
 
-class ICharacter {
+class IMateriaSource {
 	public:
-		virtual ~ICharacter() {}
-		virtual std::string const & getName() const = 0;
-		virtual void equip(AMateria* m) = 0;
-		virtual void unequip(int idx) = 0;
-		virtual void use(int idx, ICharacter& target) = 0;
+	virtual ~IMateriaSource() {}
+	virtual void learnMateria(AMateria*m) = 0;
+	virtual AMateria* createMateria(std::string const & type) = 0;
 };
 
 #endif
