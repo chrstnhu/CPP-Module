637baf20d2ec66e10c551c83b7efe79726e3165b
chrhu <chrhu@student.42.fr>
2024-10-29 16:36:15 +0100

    rm interface

--- CPP_04/ex03/AMateria.hpp
+++ CPP_04/ex03/AMateria.hpp
@@ -6,7 +6,7 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 16:59:34 by chrhu             #+#    #+#             */
-/*   Updated: 2024/10/29 12:57:00 by chrhu            ###   ########.fr       */
+/*   Updated: 2024/10/29 16:27:00 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@
 #include <iostream>
 #include <string>
 #include "ICharacter.hpp"
+#include "MateriaSource.hpp"
+#include "Ice.hpp"
 
 class AMateria {
 	protected:
@@ -36,44 +38,12 @@ class AMateria {
 		// Copy assignement operator
 		AMateria &operator=(const AMateria &other);
 		
-		std::string const & getType() const; //Returns the materia type
+		// Getters
+		std::string const & getType() const;
 
+		// Functions
 		virtual AMateria* clone() const = 0;
 		virtual void use(ICharacter& target);
-};
-
-class IMateriaSource
-{
-	public:
-		virtual ~IMateriaSource() {}
-		virtual void learnMateria(AMateria*) = 0;
-		virtual AMateria* createMateria(std::string const & type) = 0;
-};
-
-class Ice : public AMateria {
-	public:
-		Ice();
-		Ice(std::string type);
-		Ice(const AMateria &other);
-		~Ice();
-
-		Ice &operator=(const Ice &other);
-
-		AMateria* clone() const;
-		void use(ICharacter& target);
-};
-
-class Cure : public AMateria {
-	public:
-		Cure();
-		Cure(std::string type);
-		Cure(const AMateria &other);
-		~Cure();
-
-		Cure &operator=(const Cure &other);
-
-		AMateria* clone() const;
-		void use(ICharacter& target);
 };
 
 #endif
