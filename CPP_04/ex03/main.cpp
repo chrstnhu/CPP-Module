0fb0963efd7489536869efea72aaa9c9a9e419d5
chrhu <chrhu@student.42.fr>
2024-10-29 16:37:07 +0100

    add main

--- CPP_04/ex03/main.cpp
+++ CPP_04/ex03/main.cpp
@@ -6,13 +6,38 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 16:58:46 by chrhu             #+#    #+#             */
-/*   Updated: 2024/10/25 17:00:26 by chrhu            ###   ########.fr       */
+/*   Updated: 2024/10/29 16:26:47 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "AMateria.cpp"
+#include "AMateria.hpp"
+#include "Character.hpp"
+#include "ICharacter.hpp"
+#include "Cure.hpp"
+#include "Ice.hpp"
+#include "MateriaSource.hpp"
+#include "IMateriaSource.hpp"
 
 int main()
 {
+	IMateriaSource* src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+	
+	ICharacter* me = new Character("me");
+	
+	AMateria* tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
 
+	ICharacter* bob = new Character("bob");
+	
+	me->use(0, *bob);
+	me->use(1, *bob);
+	delete bob;
+	delete me;
+	delete src;
+	return 0;
 }
