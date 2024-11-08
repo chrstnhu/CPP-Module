b70970c22173aaa982013c59cef657204dfa65ff
chrhu <chrhu@student.42.fr>
2024-11-08 13:23:43 +0100

    rm comm

--- CPP_04/ex03/srcs/AMateria.cpp
+++ CPP_04/ex03/srcs/AMateria.cpp
@@ -6,7 +6,7 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 17:03:27 by chrhu             #+#    #+#             */
-/*   Updated: 2024/11/06 15:57:34 by chrhu            ###   ########.fr       */
+/*   Updated: 2024/11/08 13:23:17 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,22 +21,17 @@ AMateria::AMateria(std::string const &type) : _type(type) {
 }
 
 // Copy constructor
-AMateria::AMateria(const AMateria &other) {
-	_type = other._type;
+AMateria::AMateria(const AMateria &other) : _type(other._type) {
 }
 
 // Destructor
 AMateria::~AMateria() {
 }
 
-/*
-While assigning a Materia to another, copying the type doesn’t make
-sense ? The type of the Materia is fixed and should not be changed.
-*/
 // Copy assignement operator
 AMateria &AMateria::operator=(const AMateria &other) {
 	if (this != &other) {
-		// _type = other._type;
+		_type = other._type;
 	}
 	return *this;
 }
