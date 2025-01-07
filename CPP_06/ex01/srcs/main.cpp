3bd1a22a8b80896ee2f42917d16acb342225d5dd
chrhu <chrhu@student.42.fr>
2025-01-07 17:04:19 +0100

    add test

--- CPP_06/ex01/srcs/main.cpp
+++ CPP_06/ex01/srcs/main.cpp
@@ -18,7 +18,20 @@ int main (int ac, char **av) {
 		return 1;
 	}
 	Data *original = new Data(av[1]);
-	std::cout << "Original string: " << original->getInput() << std::endl;
+	std::cout << std::endl << "Original string: " << original->getInput() << std::endl << std::endl;
 
+	uintptr_t raw = Serializer::serialize(original);
+	std::cout << "Serialized pointer (uninptr_t): " << raw << std::endl << std::endl;
+
+	Data *deserialized = Serializer::deserialize(raw);
+	std::cout << "Deserialized string: " << deserialized->getInput() << std::endl << std::endl;
+
+	if (original == deserialized) {
+	    std::cout << BOLDGREEN << "Original and deserialized pointers are the same" << DEF << std::endl << std::endl;
+	} else {
+		std::cout << BOLDRED << "Original and deserialized pointers are different" << DEF << std::endl << std::endl;
+	}
+
+	delete original;
 	return 0;
 }
