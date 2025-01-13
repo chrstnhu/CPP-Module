7aefdf835eaf58195b904c52a95b98ff8ff974cb
chrhu <chrhu@student.42.fr>
2025-01-13 20:05:43 +0100

    add CPP09 ex00 .hpp

---
+++ CPP_09/ex00/includes/BitcoinExchange.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 19:21:39 by chrhu             #+#    #+#             */
+/*   Updated: 2025/01/13 20:01:57 by chrhu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <string>
+# include <fstream>
+# include <ctime>
+# include <sstream>
+# include <cmath>
+# include <map>
+
+# include "Colors.hpp"
+
+# endif
