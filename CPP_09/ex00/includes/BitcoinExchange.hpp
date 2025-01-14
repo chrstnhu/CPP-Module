e77bfdd8739b77ff64c69d27dff870f0f85b4511
chrhu <chrhu@student.42.fr>
2025-01-14 17:16:03 +0100

    add DataExchange

--- CPP_09/ex00/includes/BitcoinExchange.hpp
+++ CPP_09/ex00/includes/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 19:21:39 by chrhu             #+#    #+#             */
-/*   Updated: 2025/01/14 12:31:44 by chrhu            ###   ########.fr       */
+/*   Updated: 2025/01/14 17:11:21 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,22 @@
 # include <sstream>
 # include <cmath>
 # include <map>
+# include <algorithm>
 
 # include "Colors.hpp"
 
 class BitCoinExchange {
     protected :
+        // std::map <std::string, double> _startline;
         std::string _line;
         std::string _date;
         std::string _value;
+        std::string _closestKey;
+        double _closestValue;
         
     public :
+        std::map <std::string, double> _startline;
+    
         BitCoinExchange();
         BitCoinExchange(std::string &line, std::string &date, std::string &value);
         BitCoinExchange(const BitCoinExchange &other);
@@ -43,6 +49,34 @@ class BitCoinExchange {
         bool isValidDate(const std::string &date);
         bool isValidValue(const std::string &value);
         bool isPositifValue(const std::string &value);
-};
+
+        std::string getClosestKey();
         
+        void setClosestKey(std::string key);
+};
+
+class DataExchange {
+    // protected :
+        // std::map <std::string, double> _data;
+        // std::string _date;
+        // std::string _value;
+        // std::string _closestKey;
+    
+    public :
+        std::map <std::string, double> _data;
+
+        DataExchange();
+        DataExchange(std::string &line, std::string &date, std::string &value);
+        DataExchange(const DataExchange &other);
+        ~DataExchange();
+
+        // Copy assignement
+        DataExchange &operator=(const DataExchange &other);
+
+        // Setter
+        // std::string getClosestKey();
+        // void setClosestKey(std::string key);
+        // void setClosestValue(double value);
+};
+
 # endif
