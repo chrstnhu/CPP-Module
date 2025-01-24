f07a9b65474594a6012071eee38479b2e4c0fec5
chrhu <chrhu@student.42.fr>
2025-01-24 17:21:20 +0100

    add test vector

---
+++ CPP_09/ex02/srcs/main_vector.cpp
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_vector.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/16 18:00:30 by chrhu             #+#    #+#             */
+/*   Updated: 2025/01/24 17:20:44 by chrhu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <vector>
+# include <deque>
+# include "../includes/PmergeMe.hpp"
+
+void recursiveSortMaxima(std::deque<std::pair<int, int> >& pairs) {
+    if (pairs.size() <= 1) {
+        return;
+    }
+
+    size_t mid = pairs.size() / 2;
+    std::deque<std::pair<int, int> > left(pairs.begin(), pairs.begin() + mid);
+    std::deque<std::pair<int, int> > right(pairs.begin() + mid, pairs.end());
+
+    recursiveSortMaxima(left);
+    recursiveSortMaxima(right);
+
+    std::deque<std::pair<int, int> > result;
+    size_t leftIndex = 0;
+    size_t rightIndex = 0;
+
+    while (leftIndex < left.size() && rightIndex < right.size()) {
+        if (left[leftIndex].second < right[rightIndex].second) {
+            result.push_back(left[leftIndex]);
+            leftIndex++;
+        } else {
+            if (right[rightIndex].second != 0) {
+                result.push_back(right[rightIndex]);
+            }
+            rightIndex++;
+        }
+    }
+
+    while (leftIndex < left.size()) {
+        result.push_back(left[leftIndex]);
+        leftIndex++;
+    }
+    while (rightIndex < right.size()) {
+        result.push_back(right[rightIndex]);
+        rightIndex++;
+    }
+
+    pairs = result;
+}
+
+// Insert Minima with Binary Search
+void insertMinimaBinarySearch(std::vector<int>& maxima, std::vector<int>& minima) {
+
+    // Insert the first minima at the front
+    maxima.insert(maxima.begin(), minima[0]);
+
+    // Find the position of minima in the maxima vector
+    for (size_t index = 1; index < minima.size(); ++index) {
+        int target = minima[index];
+
+        std::vector<int>::iterator pos = std::lower_bound(maxima.begin(), maxima.end(), target);
+
+        maxima.insert(pos, target);
+    }
+}
+
+int main(int ac, char **av) {
+    if (ac < 2) {
+        std::cout << RED "Usage : ./PmergeMe (positif) number1 number2 number3 ... numberN" DEF << std::endl;
+    }
+    
+    try {
+        PMergeMe pairsVec(ac, av);
+        pairsVec.savePairsVec(ac, av);
+        
+        // Print before sort
+        std::cout << YELLOW "Before:  " DEF;
+        for (int i = 1; i < ac; i++) {
+            std::cout << av[i] << " ";
+        }
+        std::cout << std::endl;
+
+
+        // Try with std::deque
+        clock_t start = clock();
+        pairsVec.recursiveSortMaxima(pairsVec.getPairsVec());
+        pairsVec.insertMinimaBinarySearch(pairsVec.getPairsVec());
+        clock_t end = clock();
+
+        // Print after sort
+        std::cout << YELLOW "After:   " DEF;
+        pairsVec.printPairsVec();
+        
+        
+        double durationDeque = 1000000.0 * (end - start) / CLOCKS_PER_SEC;    
+        std::cout << YELLOW "Time to process a rang of " BOLDDEF << pairsVec.getMaximaVec().size() 
+            << YELLOW " elements with std::deque : " DEF << durationDeque << " µs" << std::endl;
+    }
+    catch (std::exception &e) {
+        std::cerr << RED "Error" << std::endl;
+        std::cerr << RED "Exception catch: " << e.what() << std::endl;
+        return 1;
+    }
+    return 0;
+}
+
