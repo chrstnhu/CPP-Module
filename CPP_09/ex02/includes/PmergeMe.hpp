f8713d963ff310302bfc3d3b3181b36bef7c8588
chrhu <chrhu@student.42.fr>
2025-01-24 16:55:54 +0100

    add Pmerge class

--- CPP_09/ex02/includes/PmergeMe.hpp
+++ CPP_09/ex02/includes/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 18:01:00 by chrhu             #+#    #+#             */
-/*   Updated: 2025/01/24 12:37:50 by chrhu            ###   ########.fr       */
+/*   Updated: 2025/01/24 16:43:56 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,42 @@
 # include <algorithm>
 # include <vector>
 # include <deque>
+# include <utility>
+
+# define BEFORE 0
+# define AFTER 1
 
 # include "Colors.hpp"
+
+class PMergeMe {
+    protected:
+        std::deque<std::pair<int, int> > _pairsDeque;
+        std::deque<int> _maxima;
+        std::deque<int> _minima;
+
+        std::vector <std::vector<int> > _pairsVec;
+        std::vector <int> _maximaVec;
+        std::vector <int> _minimaVec;
+        
+    public:
+        PMergeMe();
+        PMergeMe(int ac, char **av);
+        PMergeMe(PMergeMe const &other);
+        ~PMergeMe();
+
+        PMergeMe & operator=(PMergeMe const &other);
+
+        // Getters
+        std::deque<std::pair<int, int> > &getPairsDeque();
+        std::deque<int> &getMaxima();
+        
+        void savePairsDeque(int ac, char **av);
+        void recursiveSortMaxima(std::deque<std::pair<int, int> > &pairs);
+        void insertMinimaBinarySearch(std::deque<std::pair<int, int> >& pairs);
+        void printPairsDeque();
+        
+
+};
+
 
 # endif
