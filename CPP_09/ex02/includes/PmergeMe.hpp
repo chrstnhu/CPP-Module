deef24b7f7ea755c7c04e7c3457962d588d74ae7
chrhu <chrhu@student.42.fr>
2025-02-06 14:58:43 +0100

    hide list

--- CPP_09/ex02/includes/PmergeMe.hpp
+++ CPP_09/ex02/includes/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 18:01:00 by chrhu             #+#    #+#             */
-/*   Updated: 2025/02/05 16:53:07 by chrhu            ###   ########.fr       */
+/*   Updated: 2025/02/06 14:58:10 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,14 @@
 # include <deque>
 # include <utility>
 
-# include <list>
+// # include <list>
 
 # define BEFORE 0
 # define AFTER 1
 # define DEQUE 2
 # define VECTOR 3
-# define LIST 4
+// # define LIST 4
+# define MAXNBR 20
 
 # include "Colors.hpp"
 # include "ExceptionError.hpp"
@@ -45,13 +46,12 @@ class PMergeMe : public ExceptionError{
         std::vector <std::pair<int, int> > _pairsVec;
         std::vector <std::pair<int, int> > _impairNbrVec;
         std::vector <int> _mainVec;
-        std::vector <int> _pendingVec;        
+        std::vector <int> _pendingVec;    
 
-        std::list <std::pair<int, int> > _pairsList;
-        std::list <std::pair<int, int> > _impairNbrList;
-        std::list <int> _mainList;
-        std::list <int> _pendingList;        
-        
+        // std::list <std::pair<int, int> > _pairsList;
+        // std::list <std::pair<int, int> > _impairNbrList;
+        // std::list <int> _mainList;
+        // std::list <int> _pendingList;           
     public:
         PMergeMe();
         PMergeMe(int ac, char **av);
@@ -67,10 +67,10 @@ class PMergeMe : public ExceptionError{
         
         std::vector<std::pair<int, int> > &getPairsVec();
         std::vector<int> &getMainVec();
-
-        std::list<std::pair<int, int> > &getPairsList();
-        std::list<int> &getMainList();
-
+        
+        // std::list<std::pair<int, int> > &getPairsList();
+        // std::list<int> &getMainList();
+        
         // Methods
         void checkArgs(int ac, char **av);
         int jacobsthalNumber(int n);
@@ -89,13 +89,13 @@ class PMergeMe : public ExceptionError{
         void recursiveSortMaxima(std::vector<std::pair<int, int> > &pairsVec);
         std::vector<int> jacobsthal(std::vector<std::pair<int, int> > &pairsDeque);
         void insertMinimaBinarySearch(std::vector<int> jacobsthalDist);
-
-        // List
-        void savePairsList(int ac, char **av);
-        void sortFordJohnson(std::list<std::pair<int, int> > &pairsVec);
-        void recursiveSortMaxima(std::list<std::pair<int, int> > &pairsVec);
-        std::list<int> jacobsthal(std::list<std::pair<int, int> > &pairsDeque);
-        void insertMinimaBinarySearch(std::list<int> jacobsthalDist);
+        
+        // // List
+        // void savePairsList(int ac, char **av);
+        // void sortFordJohnson(std::list<std::pair<int, int> > &pairsVec);
+        // void recursiveSortMaxima(std::list<std::pair<int, int> > &pairsVec);
+        // std::list<int> jacobsthal(std::list<std::pair<int, int> > &pairsDeque);
+        // void insertMinimaBinarySearch(std::list<int> jacobsthalDist);
 
 };
 
