47250cca6d1a20915d445da5afd599e165d84601
chrhu <chrhu@student.42.fr>
2025-02-06 14:56:37 +0100

    add return 0

--- CPP_09/ex01/srcs/main.cpp
+++ CPP_09/ex01/srcs/main.cpp
@@ -6,17 +6,16 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 12:29:35 by chrhu             #+#    #+#             */
-/*   Updated: 2025/01/16 17:44:13 by chrhu            ###   ########.fr       */
+/*   Updated: 2025/02/04 13:44:29 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/RPN.hpp"
 
-
-
 int main(int ac, char **av) {
     if (ac != 2) {
         std::cout << RED << "Usage : ./RPN \"number and operand (type RPN)\" " << DEF << std::endl;
+        return 0;
     }
 
     try {
@@ -27,4 +26,5 @@ int main(int ac, char **av) {
     catch (std::exception &e) {
         std::cout << RED << e.what() << DEF << std::endl;
     }
+    return 0;
 }
