2b2195211e6a5121396c58d15ad4ff9f7b345929
chrhu <chrhu@student.42.fr>
2025-02-06 14:56:30 +0100

    add excpetion

--- CPP_09/ex01/includes/RPN.hpp
+++ CPP_09/ex01/includes/RPN.hpp
@@ -6,7 +6,7 @@
 /*   By: chrhu <chrhu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 12:29:51 by chrhu             #+#    #+#             */
-/*   Updated: 2025/01/29 15:37:57 by chrhu            ###   ########.fr       */
+/*   Updated: 2025/02/04 15:22:05 by chrhu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@
 class RPN {
     public :
         RPN();
-        RPN(std::stack<int> stack);
+        RPN(std::stack<double> stack);
         RPN(const RPN &other);
         ~RPN();
 
@@ -37,7 +37,7 @@ class RPN {
         
         // Function
         void checkValidity(const std::string str);
-        int evalRPN(std::string str);
+        double evalRPN(std::string str);
         
         // Exceptions
         class isInvalid: public std::exception {
@@ -52,6 +52,47 @@ class RPN {
                         "************************************\n";
                 }
         };
+
+        class notEnoughOperand: public std::exception {
+            public:
+                virtual const char* what() const throw() {
+                    return 
+                        "\n********************************\n"
+                        "*                              *\n"
+                        "*   ERROR: Not enough operand  *\n"
+                        "*     number beetween(0 - 9)   *\n"
+                        "*                              *\n"
+                        "********************************\n";
+                }
+        };
+
+        class notEnoughOperator: public std::exception {
+            public:
+                virtual const char* what() const throw() {
+                    return 
+                        "\n************************************\n"
+                        "*                                  *\n"
+                        "*    ERROR: Not enough operator    *\n"
+                        "*         sign (+, -, *, /)        *\n"
+                        "*                                  *\n"
+                        "************************************\n";
+                }
+        };
+
+
+        class invalidSize: public std::exception {
+            public:
+                virtual const char* what() const throw() {
+                    return 
+                        "\n*********************************\n"
+                        "*                               *\n"
+                        "*      ERROR: Invalid size      *\n"
+                        "*  Not enough operand in stack  *\n"
+                        "*                               *\n"
+                        "*********************************\n";
+                }
+        };
+
 };
 
 # endif
